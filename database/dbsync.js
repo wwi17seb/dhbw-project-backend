48b641a1395d2cc1e935f1d1a462c3e14e6a8b1f
Sascha <developer@wirtschaftsinformatik.se>
2020-02-10 10:12:41 +0100

    CreateDosAndUserOnStartUp

--- database/dbsync.js
+++ database/dbsync.js
@@ -1,33 +1,40 @@
 // const sequelize = require("./database");
-const db = require('../database/database');
-const userService = require('../services/userService');
-const propertiesReader = require('../tools/propertyReader');
+const db = require("../database/database");
+const directorOfStudiesService = require("../services/directorOfStudiesService");
+const authService = require("../services/authService");
+const propertiesReader = require("../tools/propertyReader");
 
+addDefaultUserAndDos = async () => {
+	const hashedPassword = await authService.hashPassword(propertiesReader.getProperty("app.defaultPassword"));
+
+	const userToCreate = {
+		username: propertiesReader.getProperty("app.defaultUser"),
+		password: hashedPassword,
+		is_admin: propertiesReader.getProperty("app.isAdmin")
+	};
+	const lecturerToCreate = {};
+	const Dos = await directorOfStudiesService.createDirectorOfStudies(userToCreate, lecturerToCreate);
+};
 // verify that db is connected
 db.sequelize
-    .authenticate()
-    .then(() => {
-        db.sequelize
-            // force: true -> drops database and add new relations
-            .sync({
-                force: true
-            })
-            // .sync()
-            .then(result => {
-                const userToCreate = {
-                    username: propertiesReader.getProperty('app.defaultUser'),
-                    password: propertiesReader.getProperty('app.defaultPassword'),
-                    isAdmin: propertiesReader.getProperty('app.isAdmin'),
-                };
-                userService.createUser(userToCreate);
-                console.log('Database successful synced');
-            })
-            .catch(err => {
-                console.log(err);
-            });
-        console.log("Database connected");
-    })
-    .catch(err => {
-        console.log('Could not connect to DB');
-        console.log("Error: " + err);
-    });
+	.authenticate()
+	.then(() => {
+		db.sequelize
+			// force: true -> drops database and add new relations
+			.sync({
+				force: true
+			})
+			// .sync()
+			.then(result => {
+				addDefaultUserAndDos();
+				console.log("Database successful synced");
+			})
+			.catch(err => {
+				console.log(err);
+			});
+		console.log("Database connected");
+	})
+	.catch(err => {
+		console.log("Could not connect to DB");
+		console.log("Error: " + err);
+	});
