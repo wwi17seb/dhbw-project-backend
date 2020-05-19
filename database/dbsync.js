0a26dac806747bb62b6d8d211cb0fa7fcc42dc22
A675239 <kai.crecelius@atos.net>
2020-05-19 09:59:00 +0200

    add testuser
    Co-authored-by: microsoft@renefischer.org <microsoft@renefischer.org>
    Co-authored-by: Niko Lockenvitz <nl@nikolockenvitz.de>

--- database/dbsync.js
+++ database/dbsync.js
@@ -2,6 +2,7 @@
 const db = require("../database/database");
 const directorOfStudiesService = require("../services/directorOfStudiesService");
 const authService = require("../services/authService");
+const userService = require("../services/userService");
 const propertiesReader = require("../tools/propertyReader");
 
 addDefaultUserAndDos = async () => {
@@ -15,6 +16,14 @@ addDefaultUserAndDos = async () => {
 	const lecturerToCreate = {};
 	const Dos = await directorOfStudiesService.createDirectorOfStudies(userToCreate, lecturerToCreate);
 };
+addTestUser = async () => {
+	const userToCreate = {
+		username: propertiesReader.getProperty("app.testUser"),
+		password: await authService.hashPassword(propertiesReader.getProperty("app.testPassword")),
+		is_admin: false
+	};
+	const testUser = await userService.createUser(userToCreate);
+};
 // verify that db is connected
 db.sequelize
 	.authenticate()
@@ -25,8 +34,9 @@ db.sequelize
 				force: true
 			})
 			// .sync()
-			.then(result => {
-				addDefaultUserAndDos();
+			.then(async (result) => {
+				await addDefaultUserAndDos();
+				await addTestUser();
 				console.log("Database successful synced");
 			})
 			.catch(err => {
