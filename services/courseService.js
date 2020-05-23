415482e4d1027175860eb329b20fbd84b57f5918
Sascha Görnert <developer@wirtschaftsinformatik.se>
2020-05-23 11:38:38 +0200

    implemented course Service

---
+++ services/courseService.js
@@ -0,0 +1,64 @@
+const db = require('../database/database');
+
+/*
+ * Returns founded course
+ */
+module.exports.findCourseById = async (courseId) => {
+  const course = await db.Course.findOne({ where: { id: courseId } });
+  return course;
+};
+
+/*
+ * Returns founded course
+ */
+module.exports.findCourseByName = async (courseName) => {
+  const course = await db.Course.findOne({ where: { name: courseName } });
+  return course;
+};
+
+// GET
+// course dann mit withMajorSubject, withSemesters, withFieldOfStudy
+/*
+ * Returns founded course
+ */
+module.exports.findAll = async (withMajorSubject, withSemesters, withFieldOfStudy) => {
+  const withInclude = [];
+  if (withMajorSubject) withInclude.push({ model: db.MajorSubject });
+  if (withSemesters) withInclude.push({ model: db.Semester });
+  if (withFieldOfStudy) withInclude.push({ model: db.FieldOfStudy });
+  const course = await db.Course.findOne({ where: { name: courseName } }, { include: withInclude });
+  return course;
+};
+
+// POST
+// name of course
+// + major subject id, DosId
+module.exports.createCourse = async (transaction, { name, majorSubjectId, directorOfStudiesId }, withMajorSubject, withDirectorOfStudies) => {
+  const withInclude = [];
+  if (withDirectorOfStudies) withInclude.push({ model: db.DirectorOfStudies });
+  if (withMajorSubject) withInclude.push({ model: db.MajorSubject });
+
+  const course = await db.Course.create({ name, createdBy_id: directorOfStudiesId }, { include: withInclude }, transaction);
+
+  return course.dataValues;
+};
+
+// PUT
+// wie post s.o.
+// receives (course) -> id, name, majorSubjectId, DoSID
+module.exports.createCourse = async (transaction, { name, majorSubjectId, directorOfStudiesId }, withMajorSubject, withDirectorOfStudies) => {
+  const withInclude = [];
+  if (withDirectorOfStudies) withInclude.push({ model: db.DirectorOfStudies });
+  if (withMajorSubject) withInclude.push({ model: db.MajorSubject });
+
+  const course = await db.Course.update({ name, createdBy_id: directorOfStudiesId }, { include: withInclude }, transaction);
+
+  return course.dataValues;
+};
+
+// Delete
+// receives (courseId, dosId)
+module.exports.deleteCourse = async (transaction, courseId) => {
+  const counter = await db.Course.destroy({ where: { id: courseId } }, transaction);
+  return counter > 0;
+};
