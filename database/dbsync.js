a0cc95137dc007a9842c86670cac797fa8032367
Sascha <developer@wirtschaftsinformatik.se>
2020-05-26 11:30:23 +0200

    fixed dbsync
    - with felix

--- database/dbsync.js
+++ database/dbsync.js
@@ -1,29 +1,22 @@
 // const sequelize = require("./database");
-const db = require("../database/database");
-const directorOfStudiesService = require("../services/directorOfStudiesService");
-const userService = require("../services/userService");
-const authService = require("../services/authService");
-const propertiesReader = require("../tools/propertyReader");
+const db = require('../database/database');
+const directorOfStudiesService = require('../services/directorOfStudiesService');
+const userService = require('../services/userService');
+const authService = require('../services/authService');
+const propertiesReader = require('../tools/propertyReader');
 
 addDefaultUserAndDos = async () => {
-  const hashedPassword = await authService.hashPassword(
-    propertiesReader.getProperty("app.defaultPassword")
-  );
+  const hashedPassword = await authService.hashPassword(propertiesReader.getProperty('app.defaultPassword'));
 
   const userToCreate = {
-    username: propertiesReader.getProperty("app.defaultUser"),
+    username: propertiesReader.getProperty('app.defaultUser'),
     password: hashedPassword,
-    is_admin: propertiesReader.getProperty("app.isAdmin"),
+    is_admin: propertiesReader.getProperty('app.isAdmin'),
   };
-  const userAvaliable = await userService.getUserByUsername(
-    userToCreate.username
-  );
+  const userAvaliable = await userService.getUserByUsername(userToCreate.username);
   if (!userAvaliable) {
     const lecturerToCreate = {};
-    await directorOfStudiesService.createDirectorOfStudies(
-      userToCreate,
-      lecturerToCreate
-    );
+    await directorOfStudiesService.createDirectorOfStudies(null, userToCreate, lecturerToCreate);
   }
 };
 // verify that db is connected
@@ -33,19 +26,18 @@ db.sequelize
     db.sequelize
       // force: true -> drops database and add new relations
       .sync({
-        force: propertiesReader.getProperty("app.forceSync"),
+        force: propertiesReader.getProperty('app.forceSync'),
       })
-      // .sync()
       .then(async (result) => {
         await addDefaultUserAndDos();
-        console.log("Database successful synced");
+        console.log('Database successful synced');
       })
       .catch((err) => {
         console.log(err);
       });
-    console.log("Database connected");
+    console.log('Database connected');
   })
   .catch((err) => {
-    console.log("Could not connect to DB");
-    console.log("Error: " + err);
+    console.log('Could not connect to DB');
+    console.log('Error: ' + err);
   });
