101947c7dc26339798f0f5eb3412db6eea89e2d9
Sascha <developer@wirtschaftsinformatik.se>
2020-05-27 13:58:48 +0200

    refactor associations and improve db creation
    Co-authored-by: microsoft@renefischer.org <microsoft@renefischer.org>
    Co-authored-by: Niko Lockenvitz <nl@nikolockenvitz.de>
    Co-authored-by: Rene Fischer <microsoft@renefischer.org>

---
+++ services/moduleGroupsService.js
@@ -0,0 +1,63 @@
+const db = require('../database/database');
+
+/*
+ * Receives moduleGroup_id
+ *
+ * Returns founded moduleGroup
+ */
+module.exports.findModuleGroupById = async (semester_id) => {
+  const moduleGroupToFind = await db.ModuleGroup.findOne({ where: { semester_id } });
+  return moduleGroupToFind;
+};
+
+/*
+ * Receives moduleGroup name
+ *
+ * Returns founded moduleGroup
+ */
+module.exports.findModuleGroupByName = async (moduleGroupName) => {
+  const moduleGroupToFind = await db.ModuleGroup.findOne({ where: { name: moduleGroupName } });
+  return moduleGroupToFind;
+};
+
+/*
+ * Returns all moduleGroups []
+ */
+module.exports.getAllModuleGroup = async () => {
+  const moduleGroup = await db.ModuleGroup.findAll();
+  return moduleGroup;
+};
+
+// POST
+/*
+ * Receives moduleGroup: { name, number_of_modules_to_attend, from_semester_number, to_semester_number }
+ * creates a new moduleGroup
+ */
+module.exports.createModuleGroup = async (transaction, { name, number_of_modules_to_attend, from_semester_number, to_semester_number }) => {
+  const moduleGroupToCreate = { name, number_of_modules_to_attend, from_semester_number, to_semester_number };
+
+  const moduleGroup = await db.ModuleGroup.create({ ...moduleGroupToCreate }, transaction);
+
+  return moduleGroup.dataValues;
+};
+
+// PUT
+/*
+ * Receives moduleGroup: { moduleGroup_id, name, number_of_modules_to_attend, from_semester_number, to_semester_number }
+ * updates a moduleGroup
+ */
+module.exports.updateModuleGroup = async (transaction, { moduleGroup_id, name, number_of_modules_to_attend, from_semester_number, to_semester_number }) => {
+  const moduleGroup = await this.findModuleGroupById(moduleGroup_id);
+  await moduleGroup.update({ name, number_of_modules_to_attend, from_semester_number, to_semester_number }, transaction);
+  return moduleGroup.dataValues;
+};
+
+// Delete
+/*
+ * Receives transaction, moduleGroup_id
+ * deletes a moduleGroup
+ */
+module.export.deleteModuleGroup = async (transaction, moduleGroup_id) => {
+  const counter = await db.ModuleGroup.destroy({ where: { moduleGroup_id } }, transaction);
+  return counter > 0;
+};
---
+++ services/semesterService.js
@@ -0,0 +1,63 @@
+const db = require('../database/database');
+
+/*
+ * Receives semesterId
+ *
+ * Returns founded semester
+ */
+module.exports.findSemesterById = async (semester_id) => {
+  const semesterToFind = await db.Semester.findOne({ where: { semester_id } });
+  return semesterToFind;
+};
+
+/*
+ * Receives semester name
+ *
+ * Returns founded semester
+ */
+module.exports.findLectureByName = async (semesterName) => {
+  const semesterToFind = await db.Semester.findOne({ where: { name: semesterName } });
+  return semesterToFind;
+};
+
+/*
+ * Returns all semesters []
+ */
+module.exports.getAllSemesters = async () => {
+  const semesters = await db.Semester.findAll();
+  return semesters;
+};
+
+// POST
+/*
+ * Receives transaction, semester: { semester_id, name, number, start_date, end_date }
+ * creates a semester
+ */
+module.exports.createSemester = async (transaction, { name, number, start_date, end_date }) => {
+  const semesterToCreate = { name, number, start_date, end_date };
+
+  const Semester = await db.Semester.create({ ...semesterToCreate }, transaction);
+
+  return Semester.dataValues;
+};
+
+// PUT
+/*
+ * Receives transaction, semester: { semester_id, name, number, start_date, end_date }
+ * updates a semester
+ */
+module.exports.updateSemester = async (transaction, { semester_id, name, number, start_date, end_date }) => {
+  const semester = await this.findSemesterById(semester_id);
+  await semester.update({ name, number, start_date, end_date }, transaction);
+  return semester.dataValues;
+};
+
+// Delete
+/*
+ * Receives transaction and semester_id
+ * deletes a semester
+ */
+module.export.deleteSemester = async (transaction, semester_id) => {
+  const counter = await db.Semester.destroy({ where: { semester_id } }, transaction);
+  return counter > 0;
+};
