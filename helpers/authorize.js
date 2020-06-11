1a95064529219fb8cacbd8349e857eb1e5b5bfcb
Sascha <developer@wirtschaftsinformatik.se>
2020-06-11 16:01:13 +0200

    add prettier
    Co-authored-by: Niko Lockenvitz <nl@nikolockenvitz.de>
    Co-authored-by: Rene Fischer <microsoft@renefischer.org>

--- helpers/authorize.js
+++ helpers/authorize.js
@@ -1,16 +1,16 @@
 const authService = require('../services/authService');
-const responseHelper = require("../helpers/responseHelper");
+const responseHelper = require('../helpers/responseHelper');
 
 module.exports = authorize;
 
-function authorize () {
+function authorize() {
   return (req, res, next) => {
     const token = authService.verifyToken(req.query.token);
     if (token) {
       req.token = token;
       next();
     } else {
-      responseHelper(res, 401, 'Unauthorized' );
+      responseHelper(res, 401, 'Unauthorized');
     }
-  }
-};
+  };
+}
