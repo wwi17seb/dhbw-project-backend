0872a217544dc022da94a7b562389ab194139e88
Sascha <developer@wirtschaftsinformatik.se>
2020-06-14 01:29:19 +0200

    implement test data for further routes moduleGroups, modules, lectures, lecturers, presentations
    Co-authored-by: Rene Fischer <microsoft@renefischer.org>
    Co-authored-by: Niko Lockenvitz <nl@nikolockenvitz.de>

--- tests/createTestData.js
+++ tests/createTestData.js
@@ -22,24 +22,16 @@ const SERVER_URL = 'http://localhost:3000';
 
 let TOKEN = null;
 const LOG_DIR = './logs';
-const verbose = process.argv.slice(2).includes('v') || process.argv.slice(2).includes('verbose');
-const saveLog = process.argv.slice(2).includes('l') || process.argv.slice(2).includes('log');
+const commandLineParameters = process.argv.slice(2);
+const verbose = commandLineParameters.includes('v') || commandLineParameters.includes('verbose');
+const saveLog = commandLineParameters.includes('l') || commandLineParameters.includes('log');
 
 if (saveLog && !fs.existsSync(LOG_DIR)) {
   fs.mkdirSync(LOG_DIR);
 }
 
-function get(path, data, token) {
-  return _fetch('GET', path, data, token);
-}
 function post(path, data, token) {
   return _fetch('POST', path, data, token);
-}
-function put(path, data, token) {
-  return _fetch('PUT', path, data, token);
-}
-function del(path, data, token) {
-  return _fetch('DELETE', path, data, token);
 }
 
 function _fetch(method, path, data, token) {
@@ -117,7 +109,7 @@ async function createAcademicRecords() {
     promiseHelper(async () => {
       data.arK = await post('academicRecords', {
         abbreviation: 'K',
-        type: 'Klaursur',
+        type: 'Klausur',
         rated: true,
       });
     }),
@@ -181,6 +173,19 @@ async function createMainFocuses() {
       });
     }),
   ]);
+}
+
+function getMainFocusIds(mainFocusKeys) {
+  const mainFocusIds = [];
+  for (let key of mainFocusKeys) {
+    if (!(key in data) && !key.startsWith('mf')) {
+      key = 'mf' + key;
+    }
+    if (key in data) {
+      mainFocusIds.push(data[key].mainFocus_id);
+    }
+  }
+  return mainFocusIds;
 }
 
 async function createFieldsOfStudyAndMajorSubjects() {
@@ -252,7 +257,7 @@ async function createFieldOfStudyDigitaleMedien() {
 async function createCourses() {
   await Promise.all([
     promiseHelper(async () => {
-      data.courseWI17SEB = await post(
+      data.courseWWI17SEB = await post(
         'courses',
         {
           name: 'WWI 17 SE B',
@@ -302,7 +307,7 @@ async function createCourses() {
       );
     }),
     promiseHelper(async () => {
-      data.courseWI17SEB = await post(
+      data.courseWWI18SEB = await post(
         'courses',
         {
           name: 'WWI 18 SE B',
@@ -354,6 +359,184 @@ async function createCourses() {
   ]);
 }
 
+async function createModuleGroupsModulesAndLectures() {
+  await Promise.all([
+    promiseHelper(async () => {
+      data.mgRechnungslegung = await post('moduleGroups', {
+        majorSubject_id: data.msWISE11.majorSubject_id,
+        name: 'Grundlagen der Rechnungslegung',
+        number_of_modules_to_attend: 2,
+        from_semester_number: 1,
+        to_semester_number: 2,
+        Modules: [
+          {
+            name: 'Grundlagen der Rechnungslegung',
+            description: '',
+            ects: 6,
+            catalog_id: 'WWISE_101',
+            academicRecord_ids: [data.arK.academicRecord_id],
+            number_of_lectures_to_attend: 2,
+            requirements: 'Keine',
+            Lectures: [
+              {
+                name: 'Finanzbuchhaltung',
+                workload_home: '36h',
+                workload_dhbw: '54h',
+                catalog_id: 'WWISE_101.1',
+                mainFocus_ids: getMainFocusIds(['BWL']),
+              },
+              {
+                name: 'Kosten- und Leistungsrechnung',
+                workload_home: '36h',
+                workload_dhbw: '54h',
+                catalog_id: 'WWISE_101.2',
+                mainFocus_ids: getMainFocusIds(['BWL']),
+              },
+            ],
+          },
+        ],
+      });
+    }),
+  ]);
+}
+
+async function createLecturers() {
+  await Promise.all([
+    promiseHelper(async () => {
+      data.lecturerTM = await post(
+        'lecturers',
+        {
+          firstname: 'Tony',
+          lastname: 'Maldonary',
+          academic_title: 'BSC.',
+          email: 'Tony.Maldonary@bachelor-of-science.com',
+          salutation: 'Herr',
+          phonenumber: '0123456789',
+          experience: 'Can handle DB lecture',
+          mainFocus_ids: getMainFocusIds(['Projektmanagement', 'Webentwicklung', 'Recht']),
+          profile: 'My Profile',
+          research: 'My Research',
+          cv: 'My CV',
+          comment: 'sehr engagiert',
+          is_extern: false,
+        },
+        data.users['gömert'].token
+      );
+    }),
+    promiseHelper(async () => {
+      data.lecturerEB = await post(
+        'lecturers',
+        {
+          firstname: 'Emmett L.',
+          lastname: 'Brown',
+          academic_title: 'Dr.',
+          email: 'emmett.l@brown.edu',
+          salutation: 'Mr.',
+          phonenumber: '+1 1210 1985 2204',
+          experience: 'time travel, DeLorean, plutionium',
+          mainFocus_ids: getMainFocusIds(['Projektmanagement', 'SoftwareEntwicklung']),
+          profile: '-',
+          research: 'lots of stuff',
+          cv: '-',
+          comment:
+            'can not hold lectures in the afternoon because he walks his dog from 3 to 5 pm; does not hesitate to cooperate with criminals',
+          is_extern: true,
+        },
+        data.users['jreichwald'].token
+      );
+    }),
+    promiseHelper(async () => {
+      data.lecturerQQ = await post(
+        'lecturers',
+        {
+          firstname: 'Quirinus',
+          lastname: 'Quirrell',
+          academic_title: '',
+          email: 'quirrell@hogwarts.co.uk',
+          salutation: 'Mr.',
+          phonenumber: '+44 578 2108',
+          experience: 'Defence Against the Dark Arts',
+          mainFocus_ids: getMainFocusIds([]),
+          profile: '-',
+          research: '',
+          cv: '-',
+          comment: 'Ravenclaw, hpmor.com',
+          is_extern: true,
+        },
+        data.users['jreichwald'].token
+      );
+    }),
+    promiseHelper(async () => {
+      data.lecturerEA = await post(
+        'lecturers',
+        {
+          firstname: 'Elliot',
+          lastname: 'Alderson',
+          academic_title: null,
+          email: 'elliotalderson@protonmail.ch',
+          salutation: 'Herr',
+          phonenumber: '(212) 555-0179',
+          experience: 'Cybersecurity, Linux',
+          mainFocus_ids: getMainFocusIds(['ITSicherheit', 'MLAIDS']),
+          profile: null,
+          research: null,
+          cv: 'Cybersecurity Engineer at Allsafe Cybersecurity',
+          comment: 'paranoid',
+          is_extern: true,
+        },
+        data.users['jreichwald'].token
+      );
+    }),
+  ]);
+}
+
+async function createPresentations() {
+  await Promise.all([
+    promiseHelper(async () => {
+      data.presentation1 = await post(
+        'presentations',
+        {
+          status: 'In Progress',
+          lecturer_id: data.lecturerQQ.lecturer_id,
+          lecture_id: data.mgRechnungslegung.Modules[0].Lectures[0].lecture_id,
+          course_id: data.courseWWI17SEB.course_id,
+          academicRecord_id: data.arK.academicRecord_id,
+          semester_id: data.courseWWI17SEB.Semesters[0].semester_id,
+        },
+        data.users['jreichwald'].token
+      );
+    }),
+    promiseHelper(async () => {
+      data.presentation2 = await post(
+        'presentations',
+        {
+          status: 'In Progress',
+          lecturer_id: data.lecturerEA.lecturer_id,
+          lecture_id: data.mgRechnungslegung.Modules[0].Lectures[0].lecture_id,
+          course_id: data.courseWWI17SEB.course_id,
+          academicRecord_id: data.arSE.academicRecord_id,
+          semester_id: data.courseWWI17SEB.Semesters[0].semester_id,
+        },
+        data.users['jreichwald'].token
+      );
+    }),
+    promiseHelper(async () => {
+      data.presentation3 = await post(
+        'presentations',
+        {
+          status: 'In Progress',
+          lecturer_id: data.lecturerEB.lecturer_id,
+          lecture_id: data.mgRechnungslegung.Modules[0].Lectures[1].lecture_id,
+          course_id: data.courseWWI18SEB.course_id,
+          academicRecord_id: data.arSE.academicRecord_id,
+          semester_id: data.courseWWI17SEB.Semesters[0].semester_id,
+        },
+        data.users['ritterbusch'].token
+      );
+    }),
+  ]);
+}
+
 const data = {};
 async function main() {
   await login();
@@ -363,7 +546,8 @@ async function main() {
     createMainFocuses(),
     createFieldsOfStudyAndMajorSubjects(),
   ]);
-  await createCourses();
+  await Promise.all([createCourses(), createModuleGroupsModulesAndLectures(), createLecturers()]),
+    await createPresentations();
 
   console.log(data.users);
 }
