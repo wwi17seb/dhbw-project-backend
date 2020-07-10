77c3848c1e28a6ffcdedf63dab7f05806c7cc712
KaiCC <kai.c.crecelius@gmx.de>
2020-07-10 12:01:52 +0200

    fixed tests/createTestData for googleCalendar

--- tests/testdata/googleCalendar.js
+++ tests/testdata/googleCalendar.js
@@ -3,8 +3,11 @@ module.exports = {
   method: 'PUT',
   data: [
     {
-      'Client-Id': '696108575064-pqp0vf98bbrklbet30qbkka6mc3io6p5.apps.googleusercontent.com',
-      'API-Key': 'AIzaSyA5OGAMBup2tHpeQvt7EA0w2zR-3ZCQ6-0',
+      id: '1',
+      data: {
+        'Client-Id': '696108575064-pqp0vf98bbrklbet30qbkka6mc3io6p5.apps.googleusercontent.com',
+        'API-Key': 'AIzaSyA5OGAMBup2tHpeQvt7EA0w2zR-3ZCQ6-0',
+      },
     },
   ],
 };
